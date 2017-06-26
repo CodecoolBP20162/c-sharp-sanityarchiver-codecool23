37ecfe29b477d6022f8dc865936764a2eb64d2bb
Daniel Zahoranszky <zahoranszky.d.milan@gmail.com>
2017-06-26 14:12:25 +0200

    Following features has been added: browse my files on drive , compress a selected file, change the file attributes in a dialog, file can be selected, compressing and decompressing of files are possible, file attributes can be changed, Files can be copied and moved from here to there, calculate and show occupied space of directories

---
+++ SanityArchive/SanityArchive/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SanityArchive
+{
+    partial class fileExp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.btnBack2 = new System.Windows.Forms.Button();
+            this.btnForward2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath2 = new System.Windows.Forms.TextBox();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(366, 15);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(54, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 46);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(408, 412);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser2.Location = new System.Drawing.Point(430, 46);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(445, 384);
+            this.webBrowser2.TabIndex = 6;
+            // 
+            // btnBack2
+            // 
+            this.btnBack2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack2.Location = new System.Drawing.Point(428, 14);
+            this.btnBack2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBack2.Name = "btnBack2";
+            this.btnBack2.Size = new System.Drawing.Size(32, 23);
+            this.btnBack2.TabIndex = 7;
+            this.btnBack2.Text = "<<";
+            this.btnBack2.UseVisualStyleBackColor = true;
+            this.btnBack2.Click += new System.EventHandler(this.btnBack2_Click);
+            // 
+            // btnForward2
+            // 
+            this.btnForward2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnForward2.Location = new System.Drawing.Point(463, 14);
+            this.btnForward2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnForward2.Name = "btnForward2";
+            this.btnForward2.Size = new System.Drawing.Size(32, 23);
+            this.btnForward2.TabIndex = 8;
+            this.btnForward2.Text = ">>";
+            this.btnForward2.UseVisualStyleBackColor = true;
+            this.btnForward2.Click += new System.EventHandler(this.btnForward2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(497, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Path";
+            // 
+            // txtPath2
+            // 
+            this.txtPath2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath2.Location = new System.Drawing.Point(528, 14);
+            this.txtPath2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPath2.Name = "txtPath2";
+            this.txtPath2.Size = new System.Drawing.Size(285, 20);
+            this.txtPath2.TabIndex = 10;
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen2.Location = new System.Drawing.Point(821, 12);
+            this.btnOpen2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(54, 23);
+            this.btnOpen2.TabIndex = 11;
+            this.btnOpen2.Text = "Open";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(115, 18);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(243, 20);
+            this.txtPath.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(78, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path";
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(45, 17);
+            this.btnForward.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(32, 23);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = ">>";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(10, 17);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(32, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // fileExp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 438);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtPath2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnForward2);
+            this.Controls.Add(this.btnBack2);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "fileExp";
+            this.Text = "SanityArchive";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button btnBack2;
+        private System.Windows.Forms.Button btnForward2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath2;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SanityArchive/SanityArchive/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchive
+{
+    public partial class fileExp : Form
+    {
+        public fileExp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog() {Description="Select your path." })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser1.Url = new Uri(fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+
+        private void btnOpen2_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog fbd2 = new FolderBrowserDialog() { Description = "Select your path." })
+            {
+                if (fbd2.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser2.Url = new Uri(fbd2.SelectedPath);
+                    txtPath2.Text = fbd2.SelectedPath;
+                }    
+            }
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                webBrowser1.GoForward();
+        }
+
+        private void btnBack2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser2.CanGoBack)
+                webBrowser2.GoBack();
+        }
+
+        private void btnForward2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser2.CanGoForward)
+                webBrowser2.GoForward();
+        }
+    }
+}

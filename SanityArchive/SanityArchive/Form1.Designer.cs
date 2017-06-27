2dd993a1b5c0523e861b1a018c9da1d5fca8c740
Daniel Zahoranszky <zahoranszky.d.milan@gmail.com>
2017-06-27 14:24:16 +0200

    The following features has been added to the project:
    Responsive fields and buttons(80%)
    Zip folder,zip file function added.
    Progress bar added for the zip feature

--- SanityArchive/SanityArchive/Form1.Designer.cs
+++ SanityArchive/SanityArchive/Form1.Designer.cs
@@ -40,6 +40,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnForward = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.btnFileName = new System.Windows.Forms.Button();
+            this.btnZipFolder = new System.Windows.Forms.Button();
+            this.btnZipFile = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // btnOpen
@@ -61,7 +70,7 @@
             this.webBrowser1.Location = new System.Drawing.Point(12, 46);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(408, 412);
+            this.webBrowser1.Size = new System.Drawing.Size(408, 349);
             this.webBrowser1.TabIndex = 5;
             // 
             // webBrowser2
@@ -72,7 +81,7 @@
             this.webBrowser2.Location = new System.Drawing.Point(430, 46);
             this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser2.Name = "webBrowser2";
-            this.webBrowser2.Size = new System.Drawing.Size(445, 384);
+            this.webBrowser2.Size = new System.Drawing.Size(445, 349);
             this.webBrowser2.TabIndex = 6;
             // 
             // btnBack2
@@ -136,7 +145,7 @@
             // 
             this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPath.Location = new System.Drawing.Point(115, 18);
+            this.txtPath.Location = new System.Drawing.Point(115, 17);
             this.txtPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtPath.Name = "txtPath";
             this.txtPath.Size = new System.Drawing.Size(243, 20);
@@ -155,7 +164,7 @@
             // 
             // btnForward
             // 
-            this.btnForward.Location = new System.Drawing.Point(45, 17);
+            this.btnForward.Location = new System.Drawing.Point(45, 15);
             this.btnForward.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnForward.Name = "btnForward";
             this.btnForward.Size = new System.Drawing.Size(32, 23);
@@ -166,7 +175,7 @@
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(10, 17);
+            this.btnBack.Location = new System.Drawing.Point(10, 15);
             this.btnBack.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(32, 23);
@@ -175,11 +184,101 @@
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(201, 402);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Folder:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(201, 436);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "File:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(266, 401);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(278, 20);
+            this.txtFolder.TabIndex = 15;
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(266, 435);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(278, 20);
+            this.txtFileName.TabIndex = 16;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(553, 399);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(30, 23);
+            this.btnFolder.TabIndex = 17;
+            this.btnFolder.Text = "...";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // btnFileName
+            // 
+            this.btnFileName.Location = new System.Drawing.Point(553, 433);
+            this.btnFileName.Name = "btnFileName";
+            this.btnFileName.Size = new System.Drawing.Size(30, 23);
+            this.btnFileName.TabIndex = 18;
+            this.btnFileName.Text = "...";
+            this.btnFileName.UseVisualStyleBackColor = true;
+            this.btnFileName.Click += new System.EventHandler(this.btnFileName_Click);
+            // 
+            // btnZipFolder
+            // 
+            this.btnZipFolder.Location = new System.Drawing.Point(589, 399);
+            this.btnZipFolder.Name = "btnZipFolder";
+            this.btnZipFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnZipFolder.TabIndex = 19;
+            this.btnZipFolder.Text = "ZIP";
+            this.btnZipFolder.UseVisualStyleBackColor = true;
+            this.btnZipFolder.Click += new System.EventHandler(this.btnZipFolder_Click);
+            // 
+            // btnZipFile
+            // 
+            this.btnZipFile.Location = new System.Drawing.Point(589, 433);
+            this.btnZipFile.Name = "btnZipFile";
+            this.btnZipFile.Size = new System.Drawing.Size(75, 23);
+            this.btnZipFile.TabIndex = 20;
+            this.btnZipFile.Text = "ZIP";
+            this.btnZipFile.UseVisualStyleBackColor = true;
+            this.btnZipFile.Click += new System.EventHandler(this.btnZipFile_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(266, 470);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(278, 23);
+            this.progressBar.TabIndex = 21;
+            // 
             // fileExp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 438);
+            this.ClientSize = new System.Drawing.Size(882, 505);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnZipFile);
+            this.Controls.Add(this.btnZipFolder);
+            this.Controls.Add(this.btnFileName);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnForward);
             this.Controls.Add(this.webBrowser2);
@@ -213,6 +312,15 @@
         private System.Windows.Forms.Button btnForward;
         private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Button btnFileName;
+        private System.Windows.Forms.Button btnZipFolder;
+        private System.Windows.Forms.Button btnZipFile;
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
 
--- SanityArchive/SanityArchive/Form1.cs
+++ SanityArchive/SanityArchive/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Compression;
+using System.Threading;
+using System.IO;
 
 namespace SanityArchive
 {
     public partial class fileExp : Form
     {
+
+        
+
         public fileExp()
         {
             InitializeComponent();
@@ -19,13 +25,17 @@ namespace SanityArchive
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            using(FolderBrowserDialog fbd = new FolderBrowserDialog() {Description="Select your path." })
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PDF Files(*.pdf)|*.pdf|WORD Files(*.doc;*.docx)|*.doc;*.docx|EXCEL Files(*.xlsx;*.xlsm;*.xlsb;*.xltx;*.xltm;*.xls;*.xlt)|*.xlsx;*.xlsm;*.xlsb;*.xltx;*.xltm;*.xls;*.xlt|Image Files(*.jpg;*.gif;*.bmp;*.png;*.jpeg)|*.jpg;*.gif;*.bmp;*.png;*.jpeg|All Files|*.*";
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() {Description="Select your path." })
             {
                 if (fbd.ShowDialog() == DialogResult.OK)
                 {
                     webBrowser1.Url = new Uri(fbd.SelectedPath);
+                    string path = fbd.ToString();
                     txtPath.Text = fbd.SelectedPath;
                 }
+                
             }
         }
 
@@ -65,6 +75,102 @@ namespace SanityArchive
         {
             if (webBrowser2.CanGoForward)
                 webBrowser2.GoForward();
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdFolder = new FolderBrowserDialog();
+            fbdFolder.Description = "Select folder";
+            if (fbdFolder.ShowDialog() == DialogResult.OK)
+                txtFolder.Text = fbdFolder.SelectedPath;
+        }
+
+        private void btnFileName_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter="All files |*.*",ValidateNames=true,Multiselect=false})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    txtFileName.Text = ofd.FileName;
+            }
+        }
+
+        private void btnZipFolder_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtFolder.Text))
+            {
+                MessageBox.Show("Please select your folder", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtFolder.Focus();
+                return;
+            }
+            string path = txtFolder.Text;
+            Thread thread = new Thread(t =>
+            {
+                using(Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile()) {
+                    zip.AddDirectory(path);
+                    System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(path);
+                    zip.SaveProgress += Zip_SaveProgress;
+                    zip.Save(string.Format("{0}{1}.zip", di.Parent.FullName, di.Name));
+                
+                }
+
+                
+            })
+            {IsBackground = true };
+            thread.Start();
+
+        }
+
+        private void Zip_SaveProgress(object sender, Ionic.Zip.SaveProgressEventArgs e)
+        {
+            if(e.EventType == Ionic.Zip.ZipProgressEventType.Saving_BeforeWriteEntry)
+            {
+                progressBar.Invoke(new MethodInvoker(delegate
+                {
+                    progressBar.Maximum = e.EntriesTotal;
+                    progressBar.Value = e.EntriesSaved + 1;
+                    progressBar.Update();
+                }));
+            }
+        }
+
+        private void Zip_SaveFileProgress(object sender, Ionic.Zip.SaveProgressEventArgs e)
+        {
+            if (e.EventType == Ionic.Zip.ZipProgressEventType.Saving_EntryBytesRead)
+            {
+                progressBar.Invoke(new MethodInvoker(delegate
+                {
+                    progressBar.Maximum =100;
+                    progressBar.Value = (int)((e.BytesTransferred * 100) / e.TotalBytesToTransfer);
+                    progressBar.Update();
+                }));
+            }
+        }
+
+        private void btnZipFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFileName.Text))
+            {
+                MessageBox.Show("Please select your filename", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtFileName.Focus();
+                return;
+            }
+            string fileName = txtFileName.Text;
+            Thread thread = new Thread(t =>
+            {
+                using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile())
+                {
+                    FileInfo fi = new FileInfo(fileName);
+                    zip.AddFile(fileName);
+                    System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(fileName);
+                    zip.SaveProgress += Zip_SaveFileProgress;
+                    zip.Save(string.Format("{0}/{1}.zip", di.Parent.FullName, fi.Name));
+
+                }
+
+
+            })
+            { IsBackground = true };
+            thread.Start();
         }
     }
 }
